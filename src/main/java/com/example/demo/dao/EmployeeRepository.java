e5536258c73d27bf00f748685e0489074aebb896
kai <carame311005@gmail.com>
2023-06-12 21:20:44 +0800

    Using Spring Data Rest

--- src/main/java/com/example/demo/dao/EmployeeRepository.java
+++ src/main/java/com/example/demo/dao/EmployeeRepository.java
@@ -2,6 +2,7 @@ package com.example.demo.dao;
 
 import com.example.demo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 public interface EmployeeRepository extends JpaRepository<Employee,Integer> {
 }
